20e77f9c6c1ef1eaa4cfd79cd3d765cedc419429
austineyapp <austineyapp96@gmail.com>
2020-12-29 18:26:41 +0800

    Initial commit

---
+++ src/sampling.py
@@ -0,0 +1,54 @@
+import numpy as np
+from torchvision import datasets, transforms
+
+def mnist_iid(dataset, num_users):
+    """
+    Sample IID client data from MNIST dataset
+    :param dataset:
+    :param num_users:
+    :return: dict of image index
+    """
+    num_items = int(len(dataset)/num_users)
+    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    for i in range(num_users):
+        dict_users[i] = set(np.random.choice(all_idxs, num_items, replace=False))
+        all_idxs = list(set(all_idxs) - dict_users[i])
+    return dict_users
+
+def mnist_noniid(dataset, num_users):
+    """
+    Sample non-IID client data from MNIST dataset
+    :param dataset:
+    :param num_users:
+    :return:
+    """
+    #60,000 training images --> 200 images/shard x 300 shards
+    num_shards, num_imgs = 200, 300
+    idx_shard = [i for i in range(num_shards)]
+    dict_users = {i: np.array([]) for i in range(num_users)}
+    idxs = np.arange(num_shards * num_imgs)
+    labels = dataset.train_labels.numpy()
+
+    #sort labels
+    idxs_labels = np.vstack((idxs, labels))
+    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+    idxs = idxs_labels[0, :]
+
+    #divide and assign 2 shards/client
+    for i in range(num_users):
+        rand_set = set(np.random.choice(idx_shard, 2, replace=False))
+        idx_shard = list(set(idx_shard) - rand_set)
+        for rand in rand_set:
+            dict_users[i] = np.concatenate(
+                (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0
+            )
+    return dict_users
+
+if __name__ == "__main__":
+    dataset_train = datasets.MNIST('/Users/austineyapp/Documents/REP/Year_4/FYP/data/mnist', train=True, download=True,
+                        transform=transforms.Compose([transforms.ToTensor(),
+                        transforms.Normalize((0.1307,),
+                                            (0.3081,))
+                                            ]))
+    num = 100
+    d = mnist_noniid(dataset_train, num)
